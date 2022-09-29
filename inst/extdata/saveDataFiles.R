b5b14457072ef238f108627b779e608ccb596972
Taryn Waite <taryn.waite17@gmail.com>
2022-09-29 13:44:12 -0400

    adding co2 sequestration back in as a param

--- inst/extdata/saveDataFiles.R
+++ inst/extdata/saveDataFiles.R
@@ -189,6 +189,7 @@ map_param_query <- tibble::tribble(
   #                                      "nonCO2 emissions by sector"),"pal_hot","no",
   "emissions","emissCO2BySectorNoBio", c("CO2 emissions by sector (no bio)", "CO2 sequestration by sector", "Land Use Change Emission (future)"),"pal_hot","no",
   "emissions","emissGHGByResProdGWPAR5", "nonCO2 emissions by resource production","pal_hot","no",
+  "emissions", "co2SequestrationBySector", "CO2 sequestration by sector", "pal_hot", "no",
   #"emissions","emissMethaneBySourceGWPAR5", "nonCO2 emissions by sector","pal_hot","no",
   #"emissions","emissByGasGWPAR5FFI", c("nonCO2 emissions by resource production","nonCO2 emissions by sector"),"pal_hot","no",
   #"emissions","emissByGasGWPAR5LUC", c("nonCO2 emissions by resource production","nonCO2 emissions by sector"),"pal_hot","no",
